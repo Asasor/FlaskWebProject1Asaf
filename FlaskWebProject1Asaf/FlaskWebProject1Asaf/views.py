6af7ec15c79711d3f102a5d31aceb541c8ac6eb5
Asasor <asaf.soreq@gmail.com>
2020-03-10 18:58:50 +0200

    fix1

--- FlaskWebProject1Asaf/FlaskWebProject1Asaf/views.py
+++ FlaskWebProject1Asaf/FlaskWebProject1Asaf/views.py
@@ -3,8 +3,21 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask   import Flask, render_template, flash, request
 from FlaskWebProject1Asaf import app
+from FlaskWebProject1Asaf.Models.QueryFormStructure import QueryFormStructure 
+from FlaskWebProject1Asaf.Models.QueryFormStructure import LoginFormStructure 
+from FlaskWebProject1Asaf.Models.QueryFormStructure import UserRegistrationFormStructure 
+from FlaskWebProject1Asaf.Models.LocalDatabaseRoutines import create_LocalDatabaseServiceRoutines
+from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms import TextField, TextAreaField, SubmitField, SelectField, DateField
+from wtforms import ValidationError
+import os
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+
+db_Functions = create_LocalDatabaseServiceRoutines() 
 
 @app.route('/')
 @app.route('/home')
@@ -45,3 +58,41 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+@app.route('/register', methods=['GET', 'POST'])
+def Register():
+    form = UserRegistrationFormStructure(request.form)
+
+    if (request.method == 'POST' and form.validate()):
+        if (not db_Functions.IsUserExist(form.username.data)):
+            db_Functions.AddNewUser(form)
+            db_table = ""
+
+            flash('Thanks for registering new user - '+ form.FirstName.data + " " + form.LastName.data )
+        else:
+            flash('Error: User with this Username already exist ! - '+ form.username.data)
+            form = UserRegistrationFormStructure(request.form)
+
+    return render_template(
+        'register.html', 
+        form=form, 
+        title='Register New User',
+        year=datetime.now().year,
+        repository_name='Pandas',
+        )
+@app.route('/login', methods=['GET', 'POST'])
+def Login():
+    form = LoginFormStructure(request.form)
+
+    if (request.method == 'POST' and form.validate()):
+        if (db_Functions.IsLoginGood(form.username.data, form.password.data)):
+            flash('Login approved!')
+        else:
+            flash('Error in - Username and/or password')
+   
+    return render_template(
+        'login.html', 
+        form=form, 
+        title='Login to data analysis',
+        year=datetime.now().year,
+        repository_name='Pandas',
+        )
