f095d0cee17cead853bbf04e8cad4884269689ff
Asasor <asaf.soreq@gmail.com>
2020-03-10 18:09:15 +0200

    update

---
+++ FlaskWebProject1Asaf/FlaskWebProject1Asaf/Models/QueryFormStructure.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class QueryFormStructure(FlaskForm):
+    """This class contains the names of all the fields in the form"""
+    name = StringField('Country Name?)' , validators = [DataRequired()])
+    submit = SubmitField('Submit')
+
+class UserRegistrationFormStructure(FlaskForm):
+    FirstName = StringField('First Name:   ' , validators = [DataRequired()])
+    LastName = StringField('Last Name:   ' , validators = [DataRequired()])
+    PhoneNum = StringField('Phone Number:   ' , validators = [DataRequired()])
+    EmailAddr = StringField('Email:   ' , validators = [DataRequired()])
+    username = StringField('User name:   ' , validators = [DataRequired()])
+    password = StringField('Pass word:   ' , validators = [DataRequired()])
+    submit = SubmitField('Submit')
+
+class LoginFormStructure(FlaskForm):
+    username = StringField('User name:   ' , validators = [DataRequired()])
+    password = StringField('Pass word:   ' , validators = [DataRequired()])
+    submit = SubmitField('Submit')
